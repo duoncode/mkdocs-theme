1ab2e84a6ed345bf3f530a5442c5eb37034924df
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-11-17 15:43:36 +0100

    Reformat code

--- docs/code/snippet.php
+++ docs/code/snippet.php
@@ -9,38 +9,38 @@ use FiveOrbs\Boiler\Engine;
 #[Call('value')]
 class Layout extends Template
 {
-    protected string $str = 'Chuck';
-    protected int $number = 666;
-    protected bool $is = false;
+	protected string $str = 'Chuck';
+	protected int $number = 666;
+	protected bool $is = false;
 
-    public function __construct(
-        #[Inject('custom-engine')]
-        Engine $engine,
-        string $path,
-        array $context,
-        protected readonly string $body,
-    ) {
-        parent::__construct($engine, $path, $context);
-    }
+	public function __construct(
+		#[Inject('custom-engine')]
+		Engine $engine,
+		string $path,
+		array $context,
+		protected readonly string $body,
+	) {
+		parent::__construct($engine, $path, $context);
+	}
 
-    /**
-     * The colors match the album cover of Scream Bloody Gore very well
-     */
-    public function body(): string
-    {
-        $s = "hans";
-        return $this->body . $this->pad($s) . $this::class;
-    }
+	/**
+	 * The colors match the album cover of Scream Bloody Gore very well
+	 */
+	public function body(): string
+	{
+		$s = "hans";
+		return $this->body . $this->pad($s) . $this::class;
+	}
 
-    protected function pad(string $value): string
-    {
-        // Strange comment
-        $text = <<<EOF
-            Images of the forgotten past
-            Your first life revealed at last
-        EOF;
+	protected function pad(string $value): string
+	{
+		// Strange comment
+		$text = <<<EOF
+			Images of the forgotten past
+			Your first life revealed at last
+		EOF;
 
-        return $text . str_pad($value, 10, '-');
-    }
+		return $text . str_pad($value, 10, '-');
+	}
 }
 
